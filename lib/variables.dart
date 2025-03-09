9964817e42685824058e536cc319a69c77fb23f8
basilio-joseph-lee <manansalajosephlee@gmail.com>
2025-03-09 09:20:45 +0800

    variables

--- lib/variables.dart
+++ lib/variables.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/cupertino.dart';
 
+bool wifiStatus = false;
+bool wifiLoading = false;
+bool wifiSwitchState = true;
+
+bool blueStatus = false;
+bool blueLoading = false;
+bool blueSwitchState = true;
+
+bool flightStatus = false;
